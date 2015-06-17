7448903338005808e70f18eeb7a25aafc5d3936e
clessmax <vuongvantruong1987@gmail.com>
2015-06-17 14:27:13 +0700

    add

---
+++ GamePikachu/src/com/example/gamepikachu/utils/BoardPikachu.java
@@ -0,0 +1,33 @@
+package com.example.gamepikachu.utils;
+
+import android.content.Context;
+
+public class BoardPikachu {
+	private int columns = 14;
+	private int rows = 9;
+	private Pikachu[][] pikachu = new Pikachu[columns][rows];
+	private BitmapManager bitmapManager = new BitmapManager();
+
+	public BoardPikachu() {
+	}
+
+	public void onCreate(Context context, int widthBoard, int heightBoard) {
+		int dwidth = widthBoard / (columns);
+		if (widthBoard / (columns) > heightBoard / (rows)) {
+			dwidth = heightBoard / (PikachuUtils.rows);
+		}
+
+		int left = (widthBoard - columns * dwidth) / 2;
+		int top = (heightBoard - rows * dwidth) / 2;
+
+		for (int coloum = 0; coloum < columns; coloum++) {
+			for (int row = 0; row < rows; row++) {
+				pikachu[coloum][row] = new Pikachu();
+				pikachu[coloum][row].create(dwidth, coloum, row, left, top);
+			}
+		}
+
+		bitmapManager.onCreate(context, dwidth);
+	}
+
+}
